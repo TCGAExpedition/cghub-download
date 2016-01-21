e4b08490d10d09940080a9902ebd4137113aa7cb
Philip D. Blood <blood@psc.edu>
2016-01-20 19:04:05 -0500

    Updates to make direct_parallel.sh work with automated cghub_download.sh script.

--- direct_parallel.sh
+++ direct_parallel.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-export TMPDIR=/local/tmp
-which gtdownload
-python_script=GT_Download_02_19_2015_3.8.7.py
-final_dir=/supercell/tcga_downloads
-key_file=/supercell/bam_gt/key_firehose6/cghub.key
-requests_dir=/supercell/bam_requests/active_downloads
-requests_file_stub=bam_status_COAD_WGS
-log_file=gtdownload
-script_dir=`pwd`
 
-threads=1
+python_script=$1
+final_dir=$2
+key_file=$3
+script_dir=$4
+requests_dir=$5
+tsv=$6
+threads=$7
+
+# extract tsv file name from full path
+local_tsv=`echo $tsv | awk -F "/" '{print $NF}'`
+# remove .tsv extension from tsv file name
+requests_file_stub=`echo $local_tsv | awk -F ".tsv" '{print $1}'`
+
 cd $requests_dir
 
 for requests_file in $requests_file_stub.x[0-9][0-9].tsv; do
-python $script_dir/$python_script -v -n "$threads" -t "$final_dir" -C "$key_file" -d "$requests_dir" $requests_file >> $script_dir/$log_file.$requests_file.log &
+python $script_dir/$python_script -v -n "$threads" -t "$final_dir" -C "$key_file" -d "$requests_dir" $requests_file >> $script_dir/gtdownload.$requests_file.log &
 sleep 10
 done
 wait
