f81d6e763a96c8f61027738a57b53818f1204d2b
Philip D. Blood <blood@psc.edu>
2016-01-20 19:02:42 -0500

    Updates to make direct_parallel_prep.sh work with automated cghub_download.sh script.

--- direct_parallel_prep.sh
+++ direct_parallel_prep.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-requests_dir=/supercell/bam_requests/active_downloads
-master_requests_file=bam_status_COAD_WGS_noheader.tsv
-requests_file_stub=bam_status_COAD_WGS
+requests_dir=$1
+tsv=$2
+max_gtdownloads=$3
 
-max_gtdownloads=6
+# extract tsv file name from full path
+local_tsv=`echo $tsv | awk -F "/" '{print $NF}'`
+# remove .tsv extension from tsv file name
+requests_file_stub=`echo $local_tsv | awk -F ".tsv" '{print $1}'`
+
+master_requests_file=${requests_file_stub}_noheader.tsv
 
 cd $requests_dir
+# make local copy of tsv file in active requests directory
+cp -p $tsv $local_tsv
+# remove header line to create tsv master file for downloads
+grep -hv pgrr_file_path $local_tsv > $master_requests_file
+
 lines=`cat $master_requests_file | wc -l`
 lines_per_file=`python -c "from math import ceil; print int(ceil($lines/${max_gtdownloads}.0))"`
 split -d -l $lines_per_file $master_requests_file
